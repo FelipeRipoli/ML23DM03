da49249f0df7ab1e7303b7765287d29ed0ba1ca0
Felipe Ripoli <feliperipoli3@gmail.com>
2023-10-03 21:29:26 -0300

    Arquivos do Projeto

--- app/src/main/java/br/edu/mouralacerda/ml23dm03/Database.kt
+++ app/src/main/java/br/edu/mouralacerda/ml23dm03/Database.kt
@@ -1,7 +1,20 @@
 package br.edu.mouralacerda.ml23dm03
 
+import android.content.Context
 import androidx.room.RoomDatabase
 
 abstract class Database: RoomDatabase() {
+    companion object{
+        private var database: Database? = null
+        private var DATABASE = "BDNOMES"
 
+        fun getInstance(context: Context): Database?{
+            if (database == null) {
+                return criaBanco(context)
+            } else {
+                return database
+            }
+        }
+
+    }
 }

b952571da9d14814b20988380de74a3da53a5be9
Felipe Ripoli <feliperipoli3@gmail.com>
2023-10-03 23:58:58 -0300

    Arquivos do Projeto Kotlin

--- app/src/main/java/br/edu/mouralacerda/ml23dm03/CadastroNomes.kt
+++ app/src/main/java/br/edu/mouralacerda/ml23dm03/CadastroNomes.kt
@@ -3,15 +3,21 @@ package br.edu.mouralacerda.ml23dm03
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class CadastroNomes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_nomes)
 
+        val texto = findViewById<EditText>(R.id.edtNome)
         val botaoSalvar = findViewById<Button>(R.id.btnSalvar)
         botaoSalvar.setOnClickListener {
-            
+            val p = Pessoa(texto.text.toString())
+
+            Database.getInstance(this)!!.pessoaDAO().salvar(p)
+
+            finish()
         }
     }
 }

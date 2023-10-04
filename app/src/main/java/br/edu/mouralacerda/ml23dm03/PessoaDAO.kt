f76c1ba671b242df9aa5a4885aa697263dd21c31
Felipe Ripoli <feliperipoli3@gmail.com>
2023-10-03 21:47:54 -0300

    Arquivos do Projeto Kotlin

--- app/src/main/java/br/edu/mouralacerda/ml23dm03/PessoaDAO.kt
+++ app/src/main/java/br/edu/mouralacerda/ml23dm03/PessoaDAO.kt
@@ -1,5 +1,14 @@
 package br.edu.mouralacerda.ml23dm03
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface PessoaDAO {
-    
+    @Insert
+    fun salvar(obj: Pessoa)
+
+    @Query("SELECT * FROM Pessoa")
+    fun listar(): List<Pessoa>
 }
